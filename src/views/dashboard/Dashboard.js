6127c925bafaf6114b5ef01c04edace4caf09406
rakan hakami <rakanhakami@rakans-MacBook-Pro-2.local>
2020-10-16 14:50:58 +0300

    rakan 6th commit

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -26,12 +26,12 @@ const Dashboard = () => {
     <>
 
       <WidgetsDropdown />
-      <CCard>
 
+      <CCard>
         <CCardBody>
           <CRow>
             <CCol sm="5">
-              <h4 id="traffic" className="card-title mb-0">Traffic(Scheduled Maintenance Plan)</h4>
+              <h4 id="plan" className="card-title mb-0">Scheduled Maintenance Plan</h4>
               <div className="small text-muted">September, 2020</div>
             </CCol>
             <CCol sm="7" className="d-none d-md-block">
@@ -62,19 +62,10 @@ const Dashboard = () => {
 
         <CCardFooter>
           <CRow className="text-center">
+
             <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">Visits</div>
-              <strong>29.703 Users (40%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                color="success"
-                value={40}
-              />
-            </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-              <div className="text-muted">Unique</div>
-              <strong>24.093 Users (20%)</strong>
+              <div className="text-muted">Plan</div>
+              <strong> 459 Relays</strong>
               <CProgress
                 className="progress-xs mt-2"
                 precision={1}
@@ -82,19 +73,21 @@ const Dashboard = () => {
                 value={40}
               />
             </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">Pageviews</div>
-              <strong>78.706 Views (60%)</strong>
+
+            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
+              <div className="text-muted">Actual</div>
+              <strong> 400 (87.15%)</strong>
               <CProgress
                 className="progress-xs mt-2"
                 precision={1}
-                color="warning"
+                color="success"
                 value={40}
               />
             </CCol>
+
             <CCol md sm="12" className="mb-sm-2 mb-0">
-              <div className="text-muted">New Users</div>
-              <strong>22.123 Users (80%)</strong>
+              <div className="text-muted">Average</div>
+              <strong> 18.18 Relay/Day </strong>
               <CProgress
                 className="progress-xs mt-2"
                 precision={1}
@@ -102,15 +95,7 @@ const Dashboard = () => {
                 value={40}
               />
             </CCol>
-            <CCol md sm="12" className="mb-sm-2 mb-0 d-md-down-none">
-              <div className="text-muted">Bounce Rate</div>
-              <strong>Average Rate (40.15%)</strong>
-              <CProgress
-                className="progress-xs mt-2"
-                precision={1}
-                value={40}
-              />
-            </CCol>
+
           </CRow>
         </CCardFooter>
       </CCard>
