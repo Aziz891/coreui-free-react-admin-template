3e9ba0d6ddab7a80ddd38d6989823a50a40943df
rakan hakami <rakanhakami@rakans-MacBook-Pro-2.local>
2020-10-28 12:43:00 +0300

    rakan 16th commit

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -54,14 +54,13 @@ const TheHeader = () => {
       </CHeaderBrand>
 
       <CHeaderNav className="d-md-down-none mr-auto">
+
         <CHeaderNavItem className="px-3">
           <CHeaderNavLink to="/dashboard"> <b>Dashboard</b> </CHeaderNavLink>
         </CHeaderNavItem>
+
         <CHeaderNavItem className="px-3" >
           <CHeaderNavLink to="/setting"> <b>Settings Analyzer</b></CHeaderNavLink>
-        </CHeaderNavItem>
-        <CHeaderNavItem  className="px-3">
-          <CHeaderNavLink to="/users"> <b>Users</b></CHeaderNavLink>
         </CHeaderNavItem>
 
       </CHeaderNav>
